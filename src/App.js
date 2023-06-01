889eaedfa3430ada2f577d68ae260d76a478066a
Aryann15 <rajlysm15@gmail.com>
2023-06-01 13:36:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,8 @@
-const user = {
-  name: "heddy lamar",
-  imageUrl: "'https://i.imgur.com/yXOvdOSs.jpg",
-  imageSize: 90,
-};
 
 function App() {
   return (
     <div className="App">
-      <h1> {user.name}</h1>
-      <img
-        classname="profilePic"
-        src={user.imageUrl}
-        alt="heddy lamar image"
-        style={{
-          width: user.imageSize,
-          height: user.imageSize,
-        }}
-      />
+      <h1> hello world</h1>
     </div>
   );
 }
